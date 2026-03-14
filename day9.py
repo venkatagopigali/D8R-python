bffe7bcdf5ed9521e26bbb9a9b1fd3b0ed336cfb
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2026-03-14 10:20:33 +0530

    git commit -m "ok"

---
+++ day9.py
@@ -0,0 +1,30 @@
+# from abc import ABC,abstractmethod
+# class pranay(ABC):
+#     # def hello(self):
+#     #     print("furthor")
+#     @abstractmethod
+#     def withdraw(self):
+#         n=1000
+#         d=int(input("enter  amount:"))
+#         if n>d:
+#             print(n-d)
+#         else:
+#             print("insufficient")
+
+# class vishnu(pranay):
+#     def withdraw(self):
+#         return super().withdraw()
+# o=vishnu()
+# o.withdraw()
+# # o1=pranay()
+# # o1.hello()
+# # o1.withdraw()
+# # print(vishnu.__mro__)
+
+
+# print(len(a))
+from user import vishnu,even_count
+a=[1,3,5,7]
+# print(len(a))
+# print(vishnu(a))
+print(even_count(a))
---
+++ user.py
@@ -0,0 +1,14 @@
+def vishnu(a):
+    c=0
+    for i in a:
+        c+=1
+    return c
+
+def even_count(a):
+    c=0
+    for i in a:
+        if i%2==0:
+            c+=1
+    return c
+    
+        
